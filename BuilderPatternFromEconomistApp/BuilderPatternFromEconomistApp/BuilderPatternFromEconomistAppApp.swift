243c013157b52a1e88bff5ebef6ec6d515d97ded
Luke Smith <lukecsmith@gmail.com>
2025-04-16 11:57:06 +0100

    Initial commit

---
+++ BuilderPatternFromEconomistApp/BuilderPatternFromEconomistApp/BuilderPatternFromEconomistAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuilderPatternFromEconomistAppApp.swift
+//  BuilderPatternFromEconomistApp
+//
+//  Created by Luke Smith on 16/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct BuilderPatternFromEconomistAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
